288b934297c93799fc78cdce26cd6b4769c93a9e
James Oliver G. Delamide <delamide.oliver@gmail.com>
2026-05-27 09:52:06 +0800

    Categories update

--- Klikk/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Klikk/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -35,7 +35,7 @@ namespace Klikk.Areas.Identity.Pages.Account
             {
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToAction("Index", "Home");
             }
         }
     }
